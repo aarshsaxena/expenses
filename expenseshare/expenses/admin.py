f5b0cdd684dbd0ee2c1359f9a0a66c4df0b07b36
aarshsaxena <iaarsh89@gmail.com>
2024-10-18 21:37:00 +0530

    database

--- expenseshare/expenses/admin.py
+++ expenseshare/expenses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import User, Expense  # Import your models
+
+# Register your models here
+admin.site.register(User)
+admin.site.register(Expense)
